cac42d5b90b6a60d75071b24c269e27c42592b46
Azat <azekeoktl@gmail.com>
2024-04-25 15:13:36 +0500

    connected to db

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	r := gin.Default()
-	r.POST("/Todo", AddTodo)
-	r.GET("/Todo", ListOfTodo)
-	r.PATCH("/Todo/{:id}", UpdateTodo)
-	r.DELETE("/Todo/{:id}", DeleteTodo)
+	// r := gin.Default()
+	// r.POST("/Todo", AddTodo)
+	// r.GET("/Todo", ListOfTodo)
+	// r.PATCH("/Todo/{:id}", UpdateTodo)
+	// r.DELETE("/Todo/{:id}", DeleteTodo)
+
+	connStr := "postgres://postgres:asdfg@localhost:5432/gopgtest?sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
 }
