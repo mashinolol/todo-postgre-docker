cd836407e781f2f4666978eaf232fea11d935baf
Azat <azekeoktl@gmail.com>
2024-04-27 21:03:51 +0500

    Creating TABLE and inserting DATA

--- main.go
+++ main.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
+type Product struct {
+	Name      string
+	Price     int
+	Available bool
+}
+
 func main() {
-	// r := gin.Default()
-	// r.POST("/Todo", AddTodo)
-	// r.GET("/Todo", ListOfTodo)
-	// r.PATCH("/Todo/{:id}", UpdateTodo)
-	// r.DELETE("/Todo/{:id}", DeleteTodo)
 
 	connStr := "postgres://postgres:asdfg@localhost:5432/gopgtest?sslmode=disable"
 
 	db, err := sql.Open("postgres", connStr)
+
+	defer db.Close()
 
 	if err != nil {
 		log.Fatal(err)
@@ -26,5 +30,38 @@ func main() {
 		log.Fatal(err)
 	}
 
-	defer db.Close()
+	createProductTable(db)
+
+	product := Product{"Book", 15, true}
+
+	pk := insertProduct(db, product)
+
+	fmt.Printf("ID: %d\n", pk)
+}
+
+func createProductTable(db *sql.DB) {
+	query := ` CREATE TABLE IF NOT EXISTS Product (
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(100) NOT NULL,
+		price NUMERIC(6,2) NOT NULL,
+		available BOOLEAN,
+		created timestamp DEFAULT NOW()
+	)`
+
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func insertProduct(db *sql.DB, product Product) int {
+	query := `INSERT INTO Product (name, price, available)
+		VALUES ($1, $2, $3) RETURNING id`
+
+	var pk int
+	err := db.QueryRow(query, product.Name, product.Price, product.Available).Scan(&pk)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return pk
 }
